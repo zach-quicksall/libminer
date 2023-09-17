c76c91636510019c333ad1a2acecdfbf425cd36a
Zach Quicksall <quicksallz@gmail.com>
2023-09-17 16:13:15 -0500

    feat: added optional lib size calculation.

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -2,13 +2,19 @@
 #'
 #' Provide a brief summary of the libraries on your machine
 #'
+#' @param sizes logical indicating whether to calculate total library size.
+#'  Default `FALSE`.
+#'
 #' @return A `data.frame` containing the count of
 #'  packages in each of the user's libraries
 #' @export
-#'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
+
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be TRUE or FALSE.")
+  }
 
   ## Get list of installed packages
   pkgs <- utils::installed.packages()
@@ -20,7 +26,18 @@ lib_summary <- function() {
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
 
+  ## Compute size of libraries (if TRUE)
+  if (sizes) {
+    pkg_df$lib_size <- sapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      }
+    )
+  }
+
   ## Return
   return(pkg_df)
+
 }
 
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -8,5 +8,12 @@ test_that("lib_summary returns expected result", {
 })
 
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument") # This is essentially a string match
+  expect_error(lib_summary("foo"), "'sizes' must be TRUE or FALSE.") # This is essentially a string match
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(names(res), c("Library","n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
+  expect_equal(ncol(res), 3)
 })
